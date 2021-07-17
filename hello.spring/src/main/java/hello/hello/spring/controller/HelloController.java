19797720ff8da7b085beba0e1eeb697e018d7d7e
hong hye ryeong <h0412l@naver.com>
2021-07-17 21:40:55 +0900

    MVC와 템플릿 엔진

--- hello.spring/src/main/java/hello/hello/spring/controller/HelloController.java
+++ hello.spring/src/main/java/hello/hello/spring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hello.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller // 컨트롤러임을 알려주는 어노테이션
 public class HelloController {
@@ -11,5 +12,12 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello"; // model의 값을 hello 화면을 실행시키도록 한다.
+    }
+
+    @GetMapping("hello-mvc")
+    // 웹 URL을 통해 파라미터를 전달 받는다.
+    public String helloMvc(@RequestParam(value = "name", re) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
