f499d2767c669575ce155f7888b90713119772dd
hong hye ryeong <h0412l@naver.com>
2021-08-12 22:05:23 +0900

    회원 서비스 구현

---
+++ hello.spring/src/main/java/hello/hello/spring/service/MemberService.java
@@ -0,0 +1,49 @@
+package hello.hello.spring.service;
+
+import hello.hello.spring.domain.Member;
+import hello.hello.spring.repository.MemberRepository;
+import hello.hello.spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // MemberRepository 인자로 받는 생성자 구현하여 동일한 메모리 객체를 사용하도록 변경하자
+    // DI: MemberService 입장에서 보면 외부에서 객체를 정해준다.
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
+    // 회원 가입
+    public long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 비즈니스 로직: 같은 이름인 회원은 안된다.
+    private void validateDuplicateMember(Member member) {
+        // 반환 값을 Optional 인자로 받아 사용할 수도 있지만,
+        // 반환받지 않고 바로 사용하는 방법을 권장한다.
+        memberRepository.findByName(member.getName())
+                .ifPresent(member1 -> { // null이 아닌 값이 존재한다면 {} 메소드 실행
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+
+        // Optional 값을 꺼낼 땐 get()을 사용하는 방법도 있지만,
+        // orElseGet()을 통해 값이 있다면 꺼내고 없다면 안에 메소드를 실행하는 방법을 많이 쓴다.
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // 회원 조회
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
---
+++ hello.spring/src/test/java/hello/hello/spring/service/MemberServiceTest.java
@@ -0,0 +1,80 @@
+package hello.hello.spring.service;
+
+import hello.hello.spring.domain.Member;
+import hello.hello.spring.repository.MemberRepository;
+import hello.hello.spring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    // MemoryMemberRepository 안 store 데이터는 static 선언이라
+    // 사용할 때마다 new()로 생성해도 무방하지만 그래도 각각 다른 객체로 관리하는 것은 좋지 않다.
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        // 오른쪽 람다를 실행할 때 왼쪽 예외가 발생하는지 체크
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+/*
+        try {
+            memberService.join(member2); // 예외 발생 체크
+            fail(); // 예외 발생이 안되면 실패 처리
+       } catch (IllegalStateException e){ // 예외 발생
+            Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }
+*/
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
