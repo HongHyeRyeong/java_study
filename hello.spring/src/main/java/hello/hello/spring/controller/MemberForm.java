43bd7e2af106275d39bf18b8a35fdff1655bcaeb
hong hye ryeong <h0412l@naver.com>
2021-09-04 18:49:28 +0900

    웹 MVC 화면 구현

---
+++ hello.spring/src/main/java/hello/hello/spring/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package hello.hello.spring.controller;
+
+public class MemberForm {
+    // createMemberForm의 name과 매핑된다.
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
